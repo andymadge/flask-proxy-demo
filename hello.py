013a5b6c49d15499e73ec042c277e7e4645d9d06
Andy Madge <andymadge@gmail.com>
2017-03-23 14:06:50 +0000

    Disabled ProxyFix for testing

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return "Your IP address is %s\n" % request.remote_addr
 
-app.wsgi_app = ProxyFix(app.wsgi_app)
+# app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
